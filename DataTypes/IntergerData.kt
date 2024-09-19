6e339de1e223d22cceb90ec29e6e31267de10094
Adebayour66265 <adebayour66265@gmail.com>
2024-09-19 07:46:42 +0100

    add variable

---
+++ DataTypes/IntergerData.kt
@@ -0,0 +1,11 @@
+package DataTypes;
+
+public class IntergerData {
+    var userAge: Int = 20;
+
+    // 1 Number: Int (whole numbers), Double or float (fraction or decimal number)
+    // 2 Text: String (more than One character)
+    // 3 Boolean: (True or False state)
+
+
+}
--- Main.kt
+++ Main.kt
@@ -1,4 +1,17 @@
-fun main(){
-    var  greeting = "Hello kotlin";
+fun main(){ //function
+    var  greeting = "Hello kotlin"; //variable
     println(greeting);
+
+    var userAge: Int = 20;
+
+
+    // How to variable
+    // 2 ways
+    //1. val: read-only
+    //2. var: mutable variables
+
+    var PI: Double = 3.1416;
+    val userName : String = "NURUDEEN MUSTAPHA";
+    val isActive : Boolean = true;
+
 }
