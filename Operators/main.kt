aa82ccc0608eeaff6a70fadc55cee861e7f97556
Adebayour66265 <adebayour66265@gmail.com>
2024-09-19 09:36:55 +0100

    add Operator

---
+++ Operators/main.kt
@@ -0,0 +1,32 @@
+package Operators
+
+        /*
+            * Operators & Expressions
+        * */
+fun main() {
+
+    //OPERATORS
+    val total = 100 + 50 //literal value
+    val result = 20 + total;
+
+            println(result);
+
+            /*
+            * TYPE OF OPERATORS
+            * -------------------
+            *   1. Arithmetic Operator
+            *   2. Assignment Operator
+            *   3. Comparison Operator
+            *   4. Logical Operator
+            * */
+
+
+
+            val applePrice = 20;
+            val OrangePrice = 4;
+
+            val fruitPrice = applePrice + OrangePrice;
+            println(fruitPrice);
+
+            println("Can we go outside ${fruitPrice}");
+}
